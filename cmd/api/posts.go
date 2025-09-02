e957a3ff253eb1a501c3a995e4bfeb567d873d63
Tiskae <adedokuntobiloba100@gmail.com>
2025-09-02 13:27:04 +0100

    feat: get post by id route & handler

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/tiskae/go-social/internal/store"
 )
 
@@ -37,5 +39,25 @@ func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request
 	if err := writeJSON(w, http.StatusCreated, post); err != nil {
 		writeJSONError(w, http.StatusInternalServerError, err.Error())
 		return
+	}
+}
+
+func (app *application) getPostByIdHandler(w http.ResponseWriter, r *http.Request) {
+	postId, err := strconv.Atoi(chi.URLParam(r, "id"))
+
+	if err != nil {
+		writeJSONError(w, http.StatusBadRequest, "post id is required as a valid integer")
+		return
+	}
+
+	post, err := app.store.Posts.GetById(r.Context(), postId)
+
+	if err != nil {
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	if err := writeJSON(w, http.StatusOK, post); err != nil {
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
 	}
 }
